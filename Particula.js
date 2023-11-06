759c151fb0a59d816734e1a8ccb538cfe5cfd6ac
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2023-11-06 16:39:33 -0500

    Actualizaciones

--- Particula.js
+++ Particula.js
@@ -1,16 +1,20 @@
 class Particula {
   constructor() {
+    this.tamanoInicial = random(10, 50);
+    this.tamano = this.tamanoInicial;
     this.estaMuerta = false;
-    this.tiempodeVida = 35;
+    this.tiempoDeVidaInicial = Math.ceil(random(50, 150));
+    this.tiempoDeVida = this.tiempoDeVidaInicial;
 
     //tiempo de vida de 10 frames por segundo
 
     this.pos = createVector(width / 2, height / 2);
 
-    this.vel = createVector(0, random(3, 10));
-    this.vel.rotate(random(360));
+    this.vel = createVector(0, random(1, 6));
+    this.vel.rotate(random(-20, 20));
 
-    this.g = createVector(0, 0.5);
+    this.g = createVector(0, -0.05);
+    this.color = color(255, 100, random(50, 255), 200);
   }
 
   //los objetos de forma interna tienen métodos
@@ -19,15 +23,29 @@ class Particula {
     this.vel.add(this.g);
     this.pos.add(this.vel);
 
-    this.tiempodeVida -= 1;
+    this.tamano = map(
+      this.tiempoDeVida,
+      0,
+      this.tiempoDeVidaInicial,
+      0,
+      this.tamanoInicial
+    );
 
-    if (this.tiempodeVida <= 0) {
+    this.tiempoDeVida -= 1;
+
+    if (this.tiempoDeVida <= 0) {
       this.estaMuerta = true;
+      print("ups, me morí!");
+      noFill();
+      //stroke(this.color);
+      strokeWeight(3);
+      circle(this.pos.x, this.pos.y, this.tamanoInicial);
     }
   }
-
   //Método = método display
   mostrar() {
-    circle(this.pos.x, this.pos.y, 20);
+    noStroke();
+    fill(this.color);
+    circle(this.pos.x, this.pos.y, this.tamano);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
-let p;
+let sistema = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  p = new Particula();
 }
-
 function draw() {
-  if (!p.estaMuerta) {
-    p.actualizar();
-    p.mostrar();
+  background(0, 50);
+
+  let p1 = new Particula();
+  sistema.push(p1);
+  let p2 = new Particula();
+  sistema.push(p2);
+  let p3 = new Particula();
+  sistema.push(p3);
+
+  for (let i = 0; i < sistema.length; i++) {
+    if (sistema[i].estaMuerta) {
+      sistema.splice(i, 1);
+    }
   }
+
+  for (let i = 0; i < sistema.length; i++) {
+    sistema[i].actualizar();
+    sistema[i].mostrar();
+  }
+
+  print(sistema.length);
 }
